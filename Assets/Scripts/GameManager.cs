2e5e7e020e0754e6759ab981ad7d8de282994683
Zaigham <razazaigham14@gmail.com>
2023-01-26 18:16:45 +0500

    Prototype Ready

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager GM;
+
+    public bool KeyCollected = false;
+    public Toggle KeyToggle;
+
+    private void Awake()
+    {
+        GM = this;
+    }
+
+    private void Update()
+    {
+        if (KeyCollected)
+        {
+            KeyToggle.isOn = true;
+        }
+    }
+}
